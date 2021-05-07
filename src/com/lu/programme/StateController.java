caeab71a1c037832fb6d30bb050215616c761e02
GeorgeLuUOL <47080847+GeorgeLuUOL@users.noreply.github.com>
2021-05-07 21:55:15 +0100

    button,label

--- src/com/lu/programme/StateController.java
+++ src/com/lu/programme/StateController.java
@@ -1,4 +1,40 @@
 package com.lu.programme;
 
+import java.util.HashMap;
+
 public class StateController {
+    State currentState;
+    HashMap<Integer,State> stateMap=new HashMap<>();
+
+    public void addState(){
+
+    }
+    public void setCurrentState(State s){
+        this.currentState=s;
+    }
+}
+class State{
+    int number;
+    String tag;
+
+    public State(int number, String tag) {
+        this.number = number;
+        this.tag = tag;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
 }
--- src/com/lu/uicomponent/ButtonHandller.java
+++ src/com/lu/uicomponent/ButtonHandller.java
@@ -1,4 +1,14 @@
 package com.lu.uicomponent;
 
+import com.lu.programme.StateController;
+
+import java.util.ArrayList;
+
 public class ButtonHandller {
+    ArrayList<Button> buttonArr=new ArrayList(10);
+    StateController stateController;
+
+    public ButtonHandller(StateController sc){
+        this.stateController=sc;
+    }
 }
