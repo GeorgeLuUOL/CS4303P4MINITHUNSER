31524984be45e81c5967e55571ec518c6569ae09
GeorgeLuUOL <47080847+GeorgeLuUOL@users.noreply.github.com>
2021-05-09 20:28:25 +0100

    levelsystem

--- src/com/lu/flight/Box.java
+++ src/com/lu/flight/Box.java
@@ -12,29 +12,36 @@ public class Box {
     int y;
     int velocity;
     int hp;
+    int type;
     Shape body;
     Texture t;
     public RigidBody r;
     PApplet p;
 
-    public Box(int x, int y, int velocity, int hp){
-        this.hp=hp;
-        this.x=x;
-        this.y=y;
-        this.velocity=velocity;
-        body=new Shape("image",0,0,50,50,"foe-fighter.png");
-        t=new Texture(this.x,this.y,body);
-        r=new RigidBody(t){
+    //for type 1 for hp, 2 for damage, 3 for maneuverability
+    public Box(int x, int y, int type) {
+        this.hp = hp;
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        if (type == 1) {
+            body = new Shape("image", 0, 0, 50, 50, "box-hp.png");
+        } else if (type == 2) {
+            body = new Shape("image", 0, 0, 50, 50, "box-damage.png");
+        }
+        t = new Texture(this.x, this.y, body);
+        r = new RigidBody(t) {
             @Override
             public void onCollision(RigidBody r) {
                 //super.onCollision(r);
-                if(r.getTag().equals("bullet")){
-                    System.out.println("hit by bullet");}
+                if (r.getTag().equals("player")) {
+                    System.out.println("collect box");
+                }
             }
         };
-        r.addColideBox(new ColideBox("rectangle",this.x,this.y,40,30,r));
+        r.addColideBox(new ColideBox("rectangle", this.x, this.y, 25, 25, r));
         r.setApplyGravity(false);
-        r.setVelocity(new PVector(-velocity,0));
-        p=r.getP();
+        r.setVelocity(new PVector(-3, -2));
+        p = r.getP();
     }
 }
